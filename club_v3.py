5f071e3ae18e262fc7fff1727d357ae76211d0ea
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-09-14 23:44:49 +0800

    '23 44 0914'

--- club_v3.py
+++ club_v3.py
@@ -37,13 +37,13 @@ def click_more_content(driver):
 	for i in post:
 		try:
 			driver.execute_script("arguments[0].scrollIntoView(false);", i)
-			time.sleep(2)
+			time.sleep(1)
 			more_content = i.find_element_by_xpath('.//div[@class="oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv'
 												   ' nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 '
 												   'a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p"]')
 			try:
 				driver.execute_script("arguments[0].scrollIntoView(false);", more_content)
-				time.sleep(2)
+				time.sleep(1)
 				more_content.click()
 			except:
 					print("FAIL CLICK")
@@ -52,101 +52,148 @@ def click_more_content(driver):
 			pass
 def make_post_dict(html_doc):
 	soup = BeautifulSoup(html_doc, 'html.parser')
-	comment_id_list=[]
-	comment_content_list=[]
-	like_id_list=[]
-	angry_id_list=[]
-	haha_id_list=[]
-	care_id_list=[]
-	love_id_list=[]
-	sad_id_list=[]
-	wow_id_list=[]
-	all_data={}
+	# post dict list
+	dataset=[]
+	comment_below_dict_list=[]
+	comment_dict_list=[]
 	label_list=[]
+	comment_below_dict={'comment id':'',
+		'comment content':'',
+		'comment link':'',
+		'comment sticker':'',
+		'comment img':''
+
+	}
 	comment_dict={
-		'comment id':comment_id_list,
-		'comment content':comment_content_list
+		'comment id':'',
+		'comment content':'',
+		'comment link':'',
+		'comment sticker':'',
+		'comment img':'',
+		'comment_below':comment_below_dict_list
 	}
-	reaction_dict={
-		'like id': like_id_list,
-		'angry id': angry_id_list,
-		'haha id': haha_id_list,
-		'care id': care_id_list,
-		'love id': love_id_list,
-		'sad id': sad_id_list,
-		'wow id': wow_id_list
-		}
-	post_dict={
-		'poster':'',
-		'post content':'',
-		'post_share_link':'',
-		'reaction':reaction_dict,
-		'comment':comment_dict
-	}
+	# reaction_dict={
+	# 	'like id': '',
+	# 	'angry id':'',
+	# 	'haha id': '',
+	# 	'care id':'',
+	# 	'love id':'',
+	# 	'sad id': '',
+	# 	'wow id': '',
+	# 	}
+
 	body = soup.find('body')
 	allpost=body.select('div[class="du4w35lb k4urcfbm l9j0dhe7 sjgh65i0"]')
 
 	print(len(allpost))
 	for post in allpost:
+		post_dict = {
+			'poster': '',
+			'post content': '',
+			'post_share_link': '',
+			'comment number': '',
+			'reaction':'',
+			'comment': comment_dict_list
+		}
 		# PO 文者 ID
-		poster = post.select('div[class="lzcic4wl"]')[0].get('aria-labelledby')
-		poster=post.select('h2[id={}]'.format(str(poster)))
-		# print(poster)
-		pattern = r'<span>(\w+)</span>'
-		poster = re.search(pattern, 'r'+str(poster))
-		print(poster.group(1))
-		post_dict['poster']=poster
-		#
-		label_str = post.select('div[class="lzcic4wl"]')[0].get('aria-describedby')
-		pattern=r'\w{3}_\w{1}_\w{2}'
-		print(label_str)
-		label_list=re.findall(pattern,str(label_str))
-		label_list.pop(0)
-		for index,label in enumerate(label_list):
-			pass
-			try:
-				print(label)
-				post_content=post.select('div[id={}]'.format(label))
-			except:
-				continue
+		# poster = post.select('div[class="lzcic4wl"]')[0].get('aria-labelledby')
+		# poster=post.select('h2[id={}]'.format(str(poster)))
+		# # print(poster)
+		# pattern = r'<span>(\w+)</span>'
+		# poster = re.search(pattern, 'r'+str(poster))
+		# # print(poster.group(1)) PO文者ID
+		# post_dict['poster']=poster.group(1)
+		# #五個標籤名 PO文資訊   label_str
+		# label_str = post.select('div[class="lzcic4wl"]')[0].get('aria-describedby')
+		# pattern=r'\w{3}_\w{1}_\w{2}'
+		# label_list=re.findall(pattern,str(label_str))
+		# if (len(label_list)!=0):
+		# 	label_list.pop(0)
+		# 	label_list.pop(2)
+		# 	for index,label in enumerate(label_list):
+		# 		pass
+		# 		post_content=post.select('div[id={}]'.format(label))
+		# 		if index==0:
+		# 			# PO文內容處理
+		# 			pattern=r'>(.*?)<'
+		# 			content_list1=re.findall(pattern,str(post_content))
+		# 			content=''.join([str(x) for x in content_list1])
+		# 			# print(content)
+		# 			post_dict['post content']=content
+		# 		if index==1:
+		# 			pattern = r'href="(.*?)"'
+		# 			# content 分享連結list
+		# 			share_link=re.findall(pattern,str(post_content))
+		# 			# print(share_link)
+		# 			post_dict['post_share_link']=share_link
+		# 			pass
+		# 		if index==2:
+		# 			pattern=r'>(\d+?) Comments<'
+		# 			comment_num=re.findall(pattern,str(post_content))
+		# 			# comment_num0 為 PO文底下留言數量
+		# 			if len(comment_num)!=0:
+		# 				post_dict['comment number']=comment_num[0]
+		# 			else:
+		# 				post_dict['comment number']=0
+		# 		print(post_dict)
+		# comment
+		comment_labellist=['div[class="l9j0dhe7 ecm0bbzt hv4rvrfc qt6c0cv9 dati1w0a lzcic4wl btwxx1t3 j83agx80"]',
+						   'div[class="kvgmc6g5 jb3vyjys rz4wbd8a qt6c0cv9 d0szoon8"]']
+		for index,label in enumerate(comment_labellist):
 			if index==0:
-				pattern1=r'>(\w+)</div>'
-				pattern2=r'>(\w+)<br>'
-				content_list1=[]
-				content_list2=[]
-				str1=''
-				#內容
-				try:
-					post_content=post_content.select('div[class="kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q"]')[0]
+				# 主留言
+				allcomment = post.select(label)
+				for comment in allcomment:
 					try:
-						content_list1=re.findall(pattern1,post_content)
+						pattern=r'div aria-label="Comment by (.*?) '
+						pattern_content = r'>(.*?)<'
+						comment_main=re.findall(pattern,str(comment))
+						comment_main_content_list=re.findall(pattern_content,str(comment))
+						comment_main_content=''.join([str(x) for x in comment_main_content_list])
+						pattern2=r'{}(Author)?(.+?)\d?\s·'.format(comment_main)
+						comment_main_content2=re.findall(pattern2,comment_main_content)[0][1]
+						print('content:{}'.format(comment_main_content2))
 					except:
-						pass
-					try:
-						content_list2=re.findall(pattern2,post_content)
-					except:
-						pass
-					content1=''.join([str(x) for x in content_list1])
-					content2=''.join([str(x) for x in content_list2])
-					pass
-				except:
-					print("FAIL GET content")
-					pass
-			if index==1:
-				try:
-					pass
-				except:
-					pass
-			if index==2:
-				try:
-					pass
-				except:
-					pass
-			if index==3:
-				try:
-					pass
-				except:
-					pass
+						print("FAIL 1 STAGE")
+						print(comment_main_content)
+			if index ==1:
+				# 留言下的留言
+				allcomment_comment = post.select(label)
+				for comment_below in allcomment_comment:
+					pattern = r'Reply by (.*?) '
+					comment_below_name_list=re.findall(pattern,str(comment_below))
+					print('comment_below_name:{}'.format(comment_below_name_list))
+					for index,comment_below_name in enumerate(comment_below_name_list):
+						try:
+							pattern_below_content = r'>(.*?)<'
+							comment_below_content_list1 = re.findall(pattern_below_content, str(comment_below))
+							comment_below_content = ''.join([str(x) for x in comment_below_content_list1])
+							pattern2 = r'{}(Author)?(.+?)\d?\s·'.format(comment_below_name)
+							comment_below_content2=re.findall(pattern2,comment_below_content[index][1])
+							print('comment_below_content:{}'.format(comment_below_content2))
+						except:
+							print("FAIL 2 STAGE")
+							print:(comment_below_content)
+
+		# dataset.append(post_dict)
+
+
+
+
+	print(dataset)
+
+
+
+
+def get_emoji_list():
+	pass
+
+
+
+
+
+
+
 
 
 
@@ -183,7 +230,7 @@ if __name__ == '__main__':
 	for i in range(post):
 		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 		time.sleep(2)
-	# click_more_comment(driver=driver)
+	click_more_comment(driver=driver)
 	# click_more_content(driver=driver)
 	htmltext = driver.page_source
 	#
