7ecf8de7049bf86d5e59f58e1bf07ccaf510f996
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-09-18 12:32:14 +0800

    '1232 0918'

--- club_v3.py
+++ club_v3.py
@@ -364,6 +364,7 @@ def get_comment_emoji_list(driver,post_index,comment_segment_path,comment_segmen
 		emoji_button=comment_emoji_stage1.find_elements_by_xpath(".//div[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l']")[0]
 		# 																		oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l
 		driver.execute_script("arguments[0].scrollIntoView(false);", emoji_button)
+
 		os.system("pause")
 		time.sleep(1)
 		emoji_button.click()
