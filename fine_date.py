b7ea99fa59a250f50d899ceea6b75574f434e949
“Jason-Duh(Home) <dushiun@gmail.com>
2020-11-16 01:51:54 +0800

    "fine_dataset_function "

--- fine_date.py
+++ fine_date.py
@@ -2,13 +2,14 @@ import json
 import upload
 import os
 import re
+import time
 def save_json_file_for_fine_dataset():
-    date_list = ['1021', '1022', '1023', '1024', '1025', '1026', '1027', '1110', '1111', '1112']
-    scratch_time = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    time_pattern = r'\d{4}-(\d{2})-(\d{2})(\s)(\d{2}):(\d{2}):\d{2}'
-    time_list = re.findall(time_pattern, scratch_time)[0]
-    finishtime = str(time_list[0] + time_list[1])
-    date_list.append(finishtime)
+    date_list = ['1021', '1022', '1023', '1024', '1025', '1026', '1027', '1110', '1111', '1112','1116']
+    # scratch_time = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    # time_pattern = r'\d{4}-(\d{2})-(\d{2})(\s)(\d{2}):(\d{2}):\d{2}'
+    # time_list = re.findall(time_pattern, scratch_time)[0]
+    # finishtime = str(time_list[0] + time_list[1])
+    # date_list.append(finishtime)
     with open('fine_dataset.json', 'w') as fp:
         json.dump(date_list, fp,indent=4)
 
